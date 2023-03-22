b511028c44e29b25f8788d0903dace23359e4f03
Eder Monteiro <emrmonteiro@inf.ufrgs.br>
2023-03-21 23:21:46 -0300

    add script to run flow with no global routing driven placement
    Signed-off-by: Eder Monteiro <emrmonteiro@inf.ufrgs.br>

---
+++ flow/util/tcc/noGRTDrivenPlace.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -x
+
+techs=("asap7" "gf180" "nangate45" "sky130hd" "sky130hs")
+
+asap7Designs=("aes" "ethmac" "ibex" "jpeg" "sha3")
+gf180Designs=("aes")
+nangate45Designs=("aes" "black_parrot" "bp_be_top" "bp_fe_top" "bp_multi_top" "ibex" "jpeg" "swerv_wrapper" "tinyRocket")
+sky130hdDesigns=("aes" "chameleon" "coyote_tc" "ibex" "jpeg" "microwatt" "riscv32i")
+sky130hsDesigns=("aes" "coyote_tc" "ibex" "jpeg" "riscv32i")
+
+for tech in "${techs[@]}"
+do
+  designs=("${asap7Designs[@]}")
+
+  case $tech in
+    "gf180")
+      designs=("${gf180Designs[@]}")
+      ;;
+    "nangate45")
+      designs=("${nangate45Designs[@]}")
+      ;;
+    "sky130hd")
+      designs=("${sky130hdDesigns[@]}")
+      ;;
+    "sky130hs")
+      designs=("${sky130hsDesigns[@]}")
+      ;;
+  esac
+
+  for design in "${designs[@]}"
+  do
+    nice test/test_helper.sh $design $tech
+    make DESIGN_CONFIG=./designs/${tech}/${design}/config.mk final_report_issue
+  done
+done
